33bff4c0b2f2d3195439597d4ce8be4b280e15e5
Kramarevaa <andrejkramarev89@gmail.com>
2020-07-01 01:10:38 +0300

    Change user_profile field to user and change Model type to OneToOneField

--- blogitproject/profiles/models.py
+++ blogitproject/profiles/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user_profile = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=150, blank=True, default='')
     about = models.TextField(default='', blank=True)
     profile_image = models.ImageField(upload_to='profile_image/%Y/%m/%d', default='default/default_profile_image.png', verbose_name='Profile Image', blank=True)
     profile_image_thumbnail = models.ImageField(upload_to='profile_image_thumbnail/%Y/%m/%d', blank=True, verbose_name='Profile Image Thumbnail')
     def __str__(self):
-        return self.user_profile.get_full_name()
+        return self.user.get_full_name()
